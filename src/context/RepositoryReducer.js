77f36bf791e6f62a26151f17e7110d8fd7962def
mazjin <mazjin55555@gmail.com>
2021-03-04 01:09:40 +0000

    remove redundant comments from reducer

--- src/context/RepositoryReducer.js
+++ src/context/RepositoryReducer.js
@@ -1,12 +1,4 @@
 export default (state, action) => {
-    // async function updateRepository(target, uri, value){
-    //     console.log(uri);
-    //     uri = uri.replace(/[/-]/g,"");
-    //     console.log(uri);
-    //     target[uri] = value;
-    //     return target;
-    // }
-
     switch(action.type) {
         case 'SET_DATA': {
             var uri = action.payload.uri.replace(/[/-]/g,"");
