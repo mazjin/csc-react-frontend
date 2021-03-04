875a118edc13112afa010f38b046786d112eb01f
mazjin <mazjin55555@gmail.com>
2021-03-04 01:09:27 +0000

    add repository provider to index

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './assets/main.css';
 import App from './App';
 import { CharacterProvider } from './context/CharacterState';
+import { RepositoryProvider } from './context/RepositoryState';
 
 ReactDOM.render(
   <React.StrictMode>
     <CharacterProvider>
-      <App />
+      <RepositoryProvider>
+        <App />
+      </RepositoryProvider>
     </CharacterProvider>
   </React.StrictMode>,
   document.getElementById('root')
